77824bff03444d7382320a5d48a08c9c453bd30b
Kelvin Eduardo <kelvin@nibbletecnologia.com.br>
2021-11-25 12:03:37 -0300

    first commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../transactionsTable";
+import { Container } from "./styles";
+
+
+export function Dashboard() {
+    return (
+        <Container>
+            
+         <Summary />
+         <TransactionsTable />
+         
+        </Container>
+    );
+}
